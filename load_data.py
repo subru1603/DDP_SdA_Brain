34c547e4a7314358b0b6b2841cb4fea3e0e27488
Suthirth <suthirthv@gmail.com>
2015-06-22 12:06:48 +0530

    Working commit of modified SdA code that loads brain patches

---
+++ load_data.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 11:44:14 2015
+
+@author: kiran
+"""
+import theano
+import theano.tensor as T
+import numpy
+import math
+import numpy as np
+
+def load_data(patch_filename, groundtruth_filename, test_filename, testtruth_filename):
+    ''' Loads the dataset
+
+    :type dataset: string
+    :param dataset: the path to the dataset (here MNIST)
+    '''
+
+    #############
+    # LOAD DATA #
+    #############
+
+
+    print '... loading data'
+
+    
+    patch_array = np.load(patch_filename)
+    groundtruth_array = np.load(groundtruth_filename)
+
+    
+    no_of_patches = patch_array.shape[0]
+    index = np.arange(no_of_patches)
+    np.random.shuffle(index)
+    
+    print(no_of_patches)
+    
+    n_trainset = int(math.floor(0.7*no_of_patches))
+    n_validset = int(math.ceil(0.3*no_of_patches))
+    
+    train_set_x = patch_array[index[0:n_trainset]]
+    train_set_y = groundtruth_array[index[0:n_trainset]]
+    
+    valid_set_x = patch_array[index[n_trainset:no_of_patches]]
+    valid_set_y = groundtruth_array[index[n_trainset:no_of_patches]]
+            
+    test_set_x = np.load(test_filename)
+    test_set_y = np.load(testtruth_filename)
+    
+    
+    print('Test set shape', test_set_x.shape[0])
+    print(valid_set_x.shape[0])
+    print(train_set_x.shape[0])
+        
+    
+    #train_set, valid_set, test_set format: tuple(input, target)
+    #input is an numpy.ndarray of 2 dimensions (a matrix)
+    #witch row's correspond to an example. target is a
+    #numpy.ndarray of 1 dimensions (vector)) that have the same length as
+    #the number of rows in the input. It should give the target
+    #target to the example with the same index in the input.
+
+    def shared_dataset(data_x, data_y, borrow=True):
+        """ Function that loads the dataset into shared variables
+
+        The reason we store our dataset in shared variables is to allow
+        Theano to copy it into the GPU memory (when code is run on GPU).
+        Since copying data into the GPU is slow, copying a minibatch everytime
+        is needed (the default behaviour if the data is not in a shared
+        variable) would lead to a large decrease in performance.
+        """
+#        data_x, data_y = data_xy
+        shared_x = theano.shared(numpy.asarray(data_x,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        shared_y = theano.shared(numpy.asarray(data_y,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        # When storing data on the GPU it has to be stored as floats
+        # therefore we will store the labels as ``floatX`` as well
+        # (``shared_y`` does exactly that). But during our computations
+        # we need them as ints (we use labels as index, and if they are
+        # floats it doesn't make sense) therefore instead of returning
+        # ``shared_y`` we will have to cast it to int. This little hack
+        # lets ous get around this issue
+        return shared_x, T.cast(shared_y, 'int32')
+
+    test_set_x, test_set_y = shared_dataset(test_set_x, test_set_y)
+    valid_set_x, valid_set_y = shared_dataset(valid_set_x, valid_set_y)
+    train_set_x, train_set_y = shared_dataset(train_set_x, train_set_y)
+
+    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y), (test_set_x, test_set_y)]
+    return rval
+
+if __name__ == '__main__':
+    load_data('Training_patches.npy','training_reshape.npy','Test_patches.npy','test_reshape.npy')
