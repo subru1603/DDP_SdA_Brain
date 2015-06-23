5937b275aee7c4749d654a737f0debdb0297c4ac
Kiran <kiranvaidhya93@gmail.com>
2015-06-24 04:57:45 +0530

    Predicts output without having to initialize network everytime

--- test_network.py
+++ test_network.py
@@ -1,10 +1,9 @@
 from test_SdA import *
 import numpy as np
 import cPickle
+import time
 
-
-def predictOutput(test_data):
-    
+def initializeNetwork():
     hidden_layers_sizes = [1000,1000,1000]
     
     ins = T.vector('ins')
@@ -12,10 +11,10 @@ def predictOutput(test_data):
     biases = T.vector('biases')
 
     hidden_activation = T.nnet.sigmoid(T.dot(ins,weights) + biases)
-    classify = T.nnet.softmax(T.dot(ins,weights) + biases)
+    classification = T.nnet.softmax(T.dot(ins,weights) + biases)
     
     activate = theano.function(inputs=[ins,weights,biases], outputs=hidden_activation, allow_input_downcast=True)
-    classification = theano.function(inputs = [ins,weights,biases], outputs=classify, allow_input_downcast = True)
+    classify = theano.function(inputs = [ins,weights,biases], outputs=classification, allow_input_downcast = True)
 
     openFile_fineTuning = open('SdAfine_tuning.pkl','rb')
     genVariables_load = cPickle.load(openFile_fineTuning)
@@ -26,14 +25,19 @@ def predictOutput(test_data):
     for i in xrange(len(hidden_layers_sizes) + 1):
         W_list.append(cPickle.load(openFile_fineTuning))
         b_list.append(cPickle.load(openFile_fineTuning))
+        
+    return activate, classify, W_list, b_list
 
+
+def predictOutput(test_data, activate, classify, W_list, b_list):
+    hidden_layers_sizes = [1000,1000,1000]
     layers = [activate(test_data,W_list[0],b_list[0])]
     
     for i in xrange(1, len(hidden_layers_sizes) + 1):
         if i < len(hidden_layers_sizes):
             layers.append(activate(layers[i-1],W_list[i],b_list[i]))
         else:
-            layers.append(classification(layers[i-1],W_list[i],b_list[i]))
+            layers.append(classify(layers[i-1],W_list[i],b_list[i]))
         
     prediction = layers[-1].argmax(axis=0)
         
@@ -45,12 +49,22 @@ def predictOutput(test_data):
         
 if __name__ == '__main__':
     
+    print 'Loading Network....'
+    _activate, _classify, _W_list, _b_list = initializeNetwork()
+    print 'Network Loaded!'
+    
     test_patches = np.load('Test_patches.npy')
     no_of_patches = test_patches.shape[0]
     pred = []
+    
+    print 'Predicting outputs....'
+    start_time = time.clock()
     for i in xrange(no_of_patches):
         test_data = test_patches[i]
-        pred.append(predictOutput(test_data=test_data))
+        pred.append(predictOutput(test_data=test_data, activate=_activate, classify=_classify, W_list=_W_list, b_list=_b_list))
+    end_time = time.clock()
+    
+    print 'Time Taken: ', ((end_time - start_time)/60.)
         
         
         
